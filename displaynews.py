92bf847b84919c839d5e2f30696be5b80bf5d2c6
Ash Inno <87377519+ashinno@users.noreply.github.com>
2023-03-22 19:21:14 +0800

    Update displaynews.py

--- displaynews.py
+++ displaynews.py
@@ -28,7 +28,7 @@ end_date = st.date_input('End date', date.today())
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
-# Allow the user to choose whether to add a 20-day moving average, Bollinger Bands, MACD, and sentiment analysis
+
 add_ma20 = st.checkbox('Add 20-day moving average')
 add_bb = st.checkbox('Add Bollinger Bands')
 add_macd = st.checkbox('Add MACD')
